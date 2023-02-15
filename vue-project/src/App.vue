1efc05f2f0afe7efc73056d1f2b1d89acf7f6be0
xiangxiongfly <xiangxiongfly@163.com>
2023-02-16 00:26:54 +0800

    asd

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,8 +4,20 @@
 		//属性绑定
 		data() {
 			return {
+				activeClass: "active",
+				pinkBg: "pinkBg",
+				classObj: {
+					active: true,
+					pinkBg: true
+				},
+				isActive: true,
+				user: {
+					name: "小白",
+					age: 18,
+					address: "beijing"
+				},
 				message: "hello world",
-				message2: "hello world",
+				message2: "hello world222",
 				num: 10,
 				uname: "小明",
 				rawHtml: '<span style="color: red">This should be red.</span>',
@@ -40,8 +52,33 @@
 		},
 		// 监听数据变化
 		watch: {
-			message2: function(newValue, oldValue) {
-				console.log(newValue, oldValue);
+			// message2: function(newValue, oldValue) {
+			// 	console.log(newValue, oldValue);
+			// 	if (newValue.length < 5 || newValue.length > 10) {
+			// 		console.log("输入不正确");
+			// 	}
+			// }
+			message2: {
+				immediate: true, //初始化时调用函数
+				handler: function(newValue) {
+					if (newValue.length < 5 || newValue.length > 10) {
+						console.log("输入不正确");
+					}
+				}
+			},
+			//深度监听，监听全部属性
+			// user: {
+			// 	handler: function(newValue) {
+			// 		console.log(newValue);
+			// 	},
+			// 	deep: true //是否深度监听
+			// }
+			//监听制定属性
+			"user.name": {
+				handler: function(newValue) {
+					console.log(newValue);
+				},
+				deep: true //是否深度监听
 			}
 		}
 	};
@@ -91,8 +128,22 @@
 	<!-- 监听器 -->
 	<p>{{message2}}</p>
 	<button @click="message2='你好'">改变message2</button>
+	<!-- 数据双向绑定 -->
+	<input type="text" v-model="message2">
+	<!-- 深度监听 -->
+	<p>{{user.name}}</p>
+	<button @click="user.name='小黑'">修改user.name</button>
 	<!-- 监听器 -->
 
+
+	<!-- 类与样式绑定 -->
+	<p class="active">hello1</p>
+	<p :class="{active:isActive,pinkBg:isActive}">hello2</p>
+	<button @click="isActive=!isActive">修改isActive</button>
+	<p :class="classObj">hello3</p>
+	<!-- 数组语法 -->
+	<p :class="[pinkBg,activeClass]">hello4</p>
+	<!-- 类与样式绑定 -->
 </template>
 
 <style>
@@ -107,5 +158,14 @@
 	.d1 {
 		font-size: 30px;
 		color: orange;
+	}
+
+	.active {
+		font-size: 30px;
+		color: red;
+	}
+
+	.pinkBg {
+		background-color: pink;
 	}
 </style>
