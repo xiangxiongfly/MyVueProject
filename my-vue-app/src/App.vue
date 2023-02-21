9d7bf658bbacc091bc16ef45026f6d9d2b01a472
xiangxiongfly <xiangxiongfly@163.com>
2023-02-21 17:59:37 +0800

    计算属性

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,77 +2,43 @@
 export default {
     data() {
         return {
-            name: "小明123",
-            age: 18,
-            num: 0,
-            rawHtml: "<h2 style='color:red;'>hello msg</h2>",
-            id: "id02",
-            imgUrl: "https://cn.vitejs.dev/logo-with-shadow.png",
-            attributeName: "id",
-            mouseEvent: "click",
+            message: "hello world",
+            num: 0
         }
     },
+    //方法
     methods: {
-        changeNum() {
-            this.num++;
-        },
-        changeColor() {
-            this.id = "id01";
-        },
-        alertMsg() {
-            alert("hello world");
+        reverseMsg2() {
+            console.log("reverseMsg2");
+            return this.message.split("").reverse().join("");
         }
     },
+    //计算属性
+    computed: {
+        reverseMsg() {
+            console.log("reverseMsg");
+            return this.message.split("").reverse().join("");
+        },
+        reverseMsg3: {
+            get() {
+                console.log("reverseMsg3");
+                return this.message.split("").reverse().join("");
+            }
+        }
+
+    }
 
 }
 </script>
 
 <template>
-    <p>姓名：{{ name }}</p>
-    <p>年龄：{{ age }}</p>
-    <p>数量：{{ num }}</p>
-
-    <!-- 仅修改一次 -->
-    <p v-once>数量：{{ num }}</p>
-    <button @click="changeNum">修改num</button>
-
-    <!-- 使用html -->
-    <p v-html="rawHtml"></p>
-
-    <!-- 动态绑定属性值 -->
-    <p v-bind:id="id">v-bind</p>
-    <button @click="changeColor">修改颜色</button>
-    <button v-on:click="changeColor">修改颜色</button>
-    <br>
-    <img v-bind:src="imgUrl" style="width: 50px;">
-
-    <!-- 使用JS表达式 -->
-    <p>{{ num + 1 }}</p>
-    <p>{{ name.split("").reverse().join("") }}</p>
-    <!-- 动态参数 -->
-    <p v-bind:[attributeName]="id">动态属性</p>
-    <button @[mouseEvent]="attributeName = 'class'">动态事件</button>
-    <button @click="mouseEvent = 'mouseover'">改变事件</button>
+    <p>{{ message }}</p>
+    <p>{{ reverseMsg2() }}</p>
+    <p>{{ reverseMsg2() }}</p>
+    <p>{{ reverseMsg }}</p>
+    <p>{{ reverseMsg }}</p>
+    <p>{{ reverseMsg3 }}</p>
+    <button @click="message = '你好'">修改message</button>
 </template>
 
-<style>
-#id01 {
-    color: red;
-}
-
-#id02 {
-    color: blue;
-}
-
-.id01 {
-    color: yellow;
-}
-
-.id02 {
-    color: yellowgreen;
-}
-
-.active {
-    color: red;
-}
-</style>
+<style></style>
