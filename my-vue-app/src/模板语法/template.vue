d0554741aa895ddd5ffb819baf5c545e135eaa92
xiangxiongfly <xiangxiongfly@163.com>
2023-02-22 16:56:38 +0800

    条件渲染和列表渲染

--- my-vue-app/src/模板语法/template.vue
+++ my-vue-app/src/模板语法/template.vue
@@ -3,7 +3,7 @@ export default {
     data() {
         return {
             name: "小明123",
-            age: 18,
+            age: 68,
             num: 0,
             rawHtml: "<h2 style='color:red;'>hello msg</h2>",
             myid: "id01",
@@ -15,6 +15,17 @@ export default {
             imgUrl: "https://cn.vitejs.dev/logo-with-shadow.png",
             attributeName: "id",
             mouseEvent: "click",
+            isShow: true,
+            userList: [
+                { name: "张三", age: 19, address: "北京" },
+                { name: "李四", age: 29, address: "上海" },
+                { name: "王五", age: 39, address: "广州" }
+            ],
+            userInfo: {
+                name: "小白",
+                title: "顶级作者",
+                bookName: "西游记"
+            }
         }
     },
     methods: {
@@ -26,6 +37,9 @@ export default {
         },
         alertMsg() {
             alert("hello world");
+        },
+        addUser() {
+            this.userList.unshift({ name: "小白", age: "8", address: "成都" })
         }
     }
 }
@@ -69,6 +83,43 @@ export default {
     <!-- 使用JavaScript表达式 -->
     <p>{{ num + 1 }}</p>
     <p>{{ name.split("").reverse().join("") }}</p>
+
+    <!-- v-if条件渲染 -->
+    <p v-if="age < 18">未成年人</p>
+    <p v-if="age >= 18 && age < 60">年轻人</p>
+    <p v-else>老人</p>
+
+    <!-- v-show，本质是display:none; -->
+    <p v-show="isShow">
+        hello template
+    </p>
+
+    <!-- v-for遍历数组 -->
+    <ul>
+        <li v-for="(item, index) in userList">
+            编号：{{ index }} 姓名：{{ item.name }} 年龄：{{ item.age }} 地址：{{ item.address }}
+        </li>
+    </ul>
+    <ul>
+        <li v-for="({ name, age, address }, index) in userList">
+            编号：{{ index }} 姓名：{{ name }} 年龄：{{ age }} 地址：{{ address }}
+        </li>
+    </ul>
+
+    <!-- v-for遍历对象 -->
+    <ul>
+        <li v-for="(value, key) in userInfo">
+            {{ key }} : {{ value }}
+        </li>
+    </ul>
+
+    <!-- :key的使用 -->
+    <ul>
+        <li v-for="item in userList" :key="item">
+            <input type="checkbox">{{ item.name }}
+        </li>
+    </ul>
+    <button @click="addUser">添加user</button>
 </template>
 
 <style>
