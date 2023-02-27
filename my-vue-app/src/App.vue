1fa6ecb94de534f4d14ad2cd0fc51321607ece98
xiangxiongfly <xiangxiongfly@163.com>
2023-02-27 09:53:36 +0800

    生命周期

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,12 +2,40 @@
 export default {
     data() {
         return {
-
+            count: 0
         }
+    },
+    beforeCreate() {
+        console.log("beforeCreate");
+    },
+    created() {
+        console.log("created");
+    },
+    beforeMount() {
+        console.log("beforeMount");
+    },
+    mounted() {
+        console.log("mounted");
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate");
+    },
+    updated() {
+        console.log("updated");
+    },
+    beforeUnmount() {
+        console.log("beforeUnmount");
+
+    },
+    unmounted() {
+        console.log("unmounted");
     }
 }
 </script>
 
-<template></template>
+<template>
+    <p>{{ count }}</p>
+    <button @click="count++">count++</button>
+</template>
 
 <style></style>
